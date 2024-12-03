f16a7c914114a632aec3ee42a55461be625c67b4
Shreyas M <65602084+Shreyas-Chinnu@users.noreply.github.com>
2024-12-03 12:48:41 +0530

    Initial project upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript to handle interactions
+
+// Example: Button click event for "Explore Services"
+document.getElementById("explore-btn").addEventListener("click", function () {
+    alert("Explore Services coming soon!");
+});
+
+
+// document.getElementById("apply-btn").addEventListener("click", function () {
+//     alert("Apply Services coming soon!");
+// });
